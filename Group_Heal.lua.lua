12ff69914ead594a4b0fdea548220f2932eca87f
Tadayoshi Carvajal Cifuentes <tadayoshicarvajalcifuentes@tadayoshis-mbp.myfiosgateway.com>
2023-09-12 23:38:34 -0400

    Add search for player with least health.

--- Group_Heal.lua.lua
+++ Group_Heal.lua.lua
@@ -8,6 +8,29 @@ function _G.OPTI_GetUnitLostHealthPercent(unit)
     end
 end
 
+
+function get_ally_lowest_health()
+    -- Gets the party member who has lost the most health so far
+    local num_group_members = GetNumGroupMembers()
+
+    local most_health_lost = 0
+    local player_with_most_health_lost = "party" .. 1
+
+    for i = 1, num_group_members do
+        local unit = "party" .. i
+        local health_lost = OPTI_GetUnitLostHealthPercent(unit)
+
+        if health_lost >= most_health_lost then
+            most_health_lost = health_lost
+            player_with_most_health_lost = unit
+        end
+    end
+
+    return player_with_most_health_lost
+
+end
+
+
 function _G.OPTI_GroupHealing(percent)
     local targetFound = false
 
@@ -21,16 +44,13 @@ function _G.OPTI_GroupHealing(percent)
     -- If the player does not fall below the threshold and he is in a group,
     -- then check the rest of the group members
     if not targetFound and IsInGroup() then
-        local numGroupMembers = GetNumGroupMembers()
+        
+        local person_to_heal = get_ally_lowest_health()
+        local health_percent = OPTI_GetUnitLostHealthPercent(person_to_heal)
 
-        for i = 1, numGroupMembers do
-            local unit = "party" .. i
-            local healthPercent = OPTI_GetUnitLostHealthPercent(unit)
-            if healthPercent >= percent then
-                UnlockedTargetUnit(unit)
-                targetFound = true
-                break
-            end
+        if health_percent >= percent then
+            UnlockedTargetUnit(person_to_heal)
+            targetFound = true
         end
     end
     return targetFound
